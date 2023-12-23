6aedb5b6b210c1aa208ddfa0bf6ad5d3e540c123
kaurjasleen240305 <jasleen_k@ee.iitr.ac.in>
2023-12-24 04:49:29 +0530

    Rehashing

--- leetcode/HashMaps/ourMap.h
+++ leetcode/HashMaps/ourMap.h
@@ -42,7 +42,15 @@ class ourmap{
    }
    
    V getValue(string key){
-
+     int bindex=getBucketIndex(key);
+     MapNode<V>* head=buckets[bindex];
+     while(head!=NULL){
+        if(head->key==key){
+        return head->value;
+        }
+        head=head->next;
+     }
+     return 0;
    }
    void insert(string key ,V val){
        int bucketIndex=getBucketIndex(key);
@@ -59,9 +67,34 @@ class ourmap{
        node->next=head;
        buckets[bucketIndex]=node;
        count++;
+       double loadFactor=(1.0*count)/numBuckets;
+       if(loadFactor>0.7){
+        rehash();
+       }
+
    }
    V remove(string key){
-
+      int bIndex=getBucketIndex(key);
+      MapNode<V> head=buckets[bIndex];
+      MapNode<V> prev=NULL;
+      while(head!=NULL){
+        if(head->key==key &){
+            if(prev==NULL){
+            buckets[bIndex]=head->next;
+        }
+        else{
+            prev->next=head->next;
+        }
+        V value=head->value;
+        head->next=NULL;
+        delete head;
+        count--;
+        return value;
+      }
+      prev=head;
+      head=head->next;
+   }
+   return 0;
    }
    private:
    int getBucketIndex(string key){
@@ -74,5 +107,29 @@ class ourmap{
         currentcoeff*=37;
     }
     return hashCode%numBuckets;
+   }
+   void reHash(){
+    MapNode<V>** temp=buckets;
+    buckets=new MapNode<V>*[2*numBuckets];
+    for(int i=0;i<(2*numBuckets);i++){
+        buckets[i]=NULL;
+    }
+    int oldBSize=numBuckets;
+    numBuckets*=2;
+    count=0;
+    for(int i=0;i<oldBSize;i++){
+        MapNode<V> head=temp[i];
+        while(head!=NULL){
+            string key=head->key;
+            V value=head->value;
+            insert(key,value);
+            head=head->next;
+        }
+        for(int i=0;i<oldBSize;i++){
+            MapNode<V> head=temp[i];
+            delete head;
+        }
+        delete[] temp;
+    }
    }
 };
