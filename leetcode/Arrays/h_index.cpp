a75b735d72706a3c3d33aa3515fabbb523f6dc85
kaurjasleen240305 <jasleen_k@ee.iitr.ac.in>
2023-12-25 05:36:15 +0530

    arr-med

---
+++ leetcode/Arrays/h_index.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        sort(citations.begin(),citations.end());
+        int x=-1;
+        for(int i=n-1;i>=0;i--){
+            if((n-i)>=citations[i]){
+                return (citations[i]);
+            }
+        }
+        return -1;
+
+    }
