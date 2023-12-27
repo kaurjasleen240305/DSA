8c6c96cf8f2718f0a2312f893d7725da71049880
kaurjasleen240305 <jasleen_k@ee.iitr.ac.in>
2023-12-27 12:10:52 +0530

    mat-med

--- leetcode/Matrix/med.cpp
+++ leetcode/Matrix/med.cpp
@@ -12,7 +12,7 @@ bool isValidSudoku(vector<vector<char>>& board) {
                     mp[{i/3,j/3}][board[i][j]]=true;
                 }
                 else{
-                    cout<<"false"<<endl;
+                    // cout<<"false"<<endl;
                     // cout<<i%3<<" "<<j%3<<" "<<board[i][j]<<endl;
                     return false;
                 }
@@ -21,16 +21,51 @@ bool isValidSudoku(vector<vector<char>>& board) {
         }
         return true;
     }
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        map<pair<int,int>,bool> mp;
+        vector<int> ans;
+        int i=0;
+        int j=0;
+        while(i<n && j<n){
+            ans.push_back(matrix[i][j]);
+            mp[{i,j}]=true;
+            // cout<<matrix[i][j]<<endl;
+            if(j+1<n && mp[{i,j+1}]==false){
+                j=j+1;
+            }
+            else if(i+1<m && mp[{i+1,j}]==false){
+                i=i+1;
+            }
+            else if(j-1>=0 && mp[{i,j-1}]==false){
+                j=j-1;
+            }
+            else if(i-1>=0 && mp[{i-1,j}]==false){
+                i=i-1;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
 int main(){
-    vector<vector<char>> v;
-    char x;
-    for(int i=0;i<9;i++){
-        vector<char> y;
-        for(int j=0;j<9;j++){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> v;
+    int x;
+    for(int i=0;i<n;i++){
+        vector<int> y;
+        for(int j=0;j<m;j++){
             cin>>x;
             y.push_back(x);
         }
         v.push_back(y);
     }
-    cout<<isValidSudoku(v)<<endl;
+    for(auto i:spiralOrder(v)){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    
 }
